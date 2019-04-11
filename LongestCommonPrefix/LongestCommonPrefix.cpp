20fea2ed2b5b8581f03d2ea222a53562a6fb8fb9
DIR\clifford.k.roberson <cliffroberson@gmail.com>
2019-04-11 12:37:35 -0500

    Created Project for Longest Common Prefix

---
+++ LongestCommonPrefix/LongestCommonPrefix.cpp
@@ -0,0 +1,13 @@
+#pragma once
+#include "pch.h"
+#include <iostream>
+#include "Solution.h"
+
+
+int main()
+{
+	cout << "good";
+
+	return 0;
+}
+
---
+++ LongestCommonPrefix/Solution.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "../std_lib_facilities.h"
+class Solution {
+public:
+	string longestCommonPrefix(vector<string>& strs) {
+
+	}
+};
