105836bf71f0a401d81da6edfaed5fc17359e82d
DIR\clifford.k.roberson <cliffroberson@gmail.com>
2019-04-11 14:52:01 -0500

    first shot on longestCommonPrefix

--- LongestCommonPrefix/LongestCommonPrefix.cpp
+++ LongestCommonPrefix/LongestCommonPrefix.cpp
@@ -6,8 +6,35 @@
 
 int main()
 {
-	cout << "good";
+	vector<string> input;
+	input.push_back("flower");
+	input.push_back("flow");
+	input.push_back("flight");
 
+	if (Solution().longestCommonPrefix(input) == "fl")
+	{
+		cout << "first OK";
+	}
+
+	input.clear();
+	input.push_back("dog");
+	input.push_back("racecar");
+	input.push_back("car");
+
+	if (Solution().longestCommonPrefix(input) == "")
+	{
+		cout << "second OK";
+	}
+
+	input.clear();
+	input.push_back("dog");
+	input.push_back("do");
+	input.push_back("car");
+
+	if (Solution().longestCommonPrefix(input) == "")
+	{
+		cout << "third OK";
+	}
 	return 0;
 }
 
--- LongestCommonPrefix/Solution.h
+++ LongestCommonPrefix/Solution.h
@@ -2,7 +2,39 @@
 #include "../std_lib_facilities.h"
 class Solution {
 public:
-	string longestCommonPrefix(vector<string>& strs) {
+	string longestCommonPrefix(vector<string>& strs) 
+	{
+		
 
+		ostringstream os;
+		string prefix = os.str();
+
+		int indexOfShortestString = 0;
+		int shortestStringSize = strs[0].size();
+		for (int i = 0; i < strs.size(); i++)
+		{
+			if (strs[i].size() < shortestStringSize)
+			{
+				shortestStringSize = strs[i].size();
+				indexOfShortestString = i;
+			}
+		}
+
+		for (int i = 0; i < strs[indexOfShortestString].size(); i++)//'i' is what character we are examining
+		{	
+			char currChar = strs[indexOfShortestString].at(i);
+			
+			for (int j = 0; j < strs.size(); j++)// 'j' is which string we are examining
+			{
+				if (strs[j][i] != currChar)
+				{
+					return prefix;
+				}
+			}
+			os << currChar;
+			prefix = os.str();
+		}
+
+		return prefix;
 	}
 };
